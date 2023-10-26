4139c10576e1588a4d1ab9b210b6e6018180e6df
Zbiku <Zbiku@MSI>
2023-10-26 22:44:50 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        double a, b, wynik;
+        string znak;
+        Console.WriteLine("Podaj a: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj b: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj znak operacji (+, -, * lub /: ");
+        znak = Console.ReadLine();
+        if(znak=="+")
+        {
+            wynik = a + b;
+            Console.WriteLine(wynik);
+        }
+        else if (znak == "-")
+        {
+            wynik = a - b;
+            Console.WriteLine(wynik);
+        }
+        else if (znak == "*")
+        {
+            wynik = a * b;
+            Console.WriteLine(wynik);
+        }
+        else if (znak == "/")
+        {
+            wynik = a / b;
+            Console.WriteLine(wynik);
+        }
+        else
+        {
+            Console.WriteLine("Podano zły znak!");
+        }
+        Console.ReadKey();
+    }
+}
